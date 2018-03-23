8a0b0241646642ebfd85618581b8d85f362855de
Sempailik <jura.v@email.cy>
2018-03-23 22:02:23 +0100

    asi dobre

--- src/rotacni_encoder_4th.ino
+++ src/rotacni_encoder_4th.ino
@@ -10,7 +10,7 @@
   *  obvod kruhu = 0,09424777m
   *  1 metr / obvod kruhu = otacky  => 1 / 0,09424777 = 10,610329
   *  otacky * pocet pulzu na otacku =>  10,610329 * 200 = 2122,06590789 pulzu na 1m
-  *  draha na 1 pulz 1 / 1061,0329 => 0,00047123889803 pulz/m
+  *  draha na 1 pulz 1 / 2122.06590789 => 0,00047123889803 pulz/m
   *
   *  pulz_na_metr = 0.00047123889803
   *
@@ -22,6 +22,7 @@
 #include "interval.h"
 
 #define pulz_na_metr 0.00047123889803
+#define pulz_na_metr_per_mil 471.23889803846
 #define pinKanalA 2         //pin 2
 #define pinKanalB 3         //pin 4   //puvodne 3
 #define pinPreruseni 0      // 0 jako INT0 coz = pin 2
@@ -35,8 +36,12 @@
 //#define tlacitko_stop 7      //pin 7
 #define dobaPoKtereZhasneLed 500 //ms
 #define casMereni  120000 //cas po ktery se bude merit v milisekundach 120000 = 120s = 2min
-#define periodaMereni 5  //4ms perioda mereni a odesilani na seriovy port ve vysledku je to 5ms
+#define periodaMereni 10  //4ms perioda mereni a odesilani na seriovy port ve vysledku je to 5ms
 #define rychlost_zmeny_mereni 2.1 //rychlost po ktere se zacne merit poctem pulzu, do te doby merit  periodou mezi pulzy
+#define korekce1 1.0575   //korekce do rychlosti 0.9m/s je 5.75% = 1.0575
+#define korekce2 1.12   //korekce 0.25m/s do rychlosti 0.9m/s je 12.0% = 1.12
+#define korekcni_hranice 0.8
+#define korekcni_hranice_spodni 0.25
 
 Interval timer,timer_led;
 
@@ -51,7 +56,7 @@ Interval timer,timer_led;
 
 // pomocné proměnné
 short int flag_interrupt = 0;
-int velikost_pwm = 204;  //hodnota  0-255,  60%=153, 70%=178, 80%=204, 90%=229,
+int velikost_pwm = 234;  //hodnota  0-255,  60%=153, 70%=178, 80%=204, 90%=229,
 int velikost_pwm_mene = 160;
 int velikost_pwm_previjeni = 229;
 volatile short int pocetPulzu = 0;
@@ -79,14 +84,18 @@ bool predchoziB = false;
 bool aktualniB = false;
 unsigned long cas_pulzu = 0;
 unsigned long predchozi_cas_pulzu = 0;
+unsigned long celkova_delka_pulzu = 0;
 unsigned long delka_pulzu = 0;
 float delkova_rychlost = 0;
+float tmp_delkova_rychlost = 0;
+unsigned short pompulz = 0;
+short int my_pocetPulzu = 0;
 
 void setup() {
   // komunikace po sériové lince rychlostí 9600 baud
   //Serial.begin(9600);
-  //Serial.begin(115200);
-  Serial.begin(250000);
+  Serial.begin(115200);
+  //Serial.begin(250000);
   //Serial.begin(76800);
   //Serial.begin(38400);
   // nastavení směru vstupního pinu
@@ -158,13 +167,32 @@ void loop() {
   if ( (timer.expired()) && flag_merit  )
   {
     unsigned long uplynulyCas = (millis() - staryCas);
-    short int my_pocetPulzu = 0;
+
+    unsigned long tmp_celkova_delka_pulzu;
+
 
     do                              //bezpecne predani hodnot z pulzru, tak aby nedoslo zrovna ke zmene
     {                               //hodnoty, pri predavani nebo vypoctu a neni potreba zakazovat preruseni
       flag_interrupt=0;             //tim padem neprijdu o data z pulzru
+/*
+      if(pompulz > 3)
+      {
+        my_pocetPulzu = pocetPulzu;
+        pompulz = 0;
+      }
+      else
+      {
+        my_pocetPulzu = 0;
+        pompulz = 0;
+      }
+*/
       my_pocetPulzu = pocetPulzu;
+      pompulz = 0;
+
       celkovy_pocet_pulzu += pocetPulzu;
+      tmp_delkova_rychlost = delkova_rychlost;
+      tmp_celkova_delka_pulzu = celkova_delka_pulzu;
+      celkova_delka_pulzu = 0;
       pocetPulzu = 0;
     } while (flag_interrupt==1);
 
@@ -181,10 +209,17 @@ void loop() {
     //rychlost = ( my_pocetPulzu * pulz_na_metr ) / ((uplynulyCas)/1000.0 ) ;
     rychlost = ( my_pocetPulzu * pulz_na_metr * 1000.0) / (uplynulyCas) ; //snad rychlejsi vypocet, protoze misto deleni nasobim
     //rychlost = ( my_pocetPulzu * 3.184713375 ) / (millis() - staryCas)   ;
+    if(rychlost < korekcni_hranice)
+    {
+      rychlost *= korekce1;
+    }
+
     if(smer==1)
       {
         if (rychlost != 0)rychlost *= -1;
       }
+
+
 
     char rychlost_temp[6];
     //char otacky_temp[7];
@@ -201,15 +236,26 @@ void loop() {
         delka += 47 * my_pocetPulzu; //
       }
 
-    delkova_rychlost = pulz_na_metr / (delka_pulzu/1000000.0);
+    //tmp_delkova_rychlost = pulz_na_metr / (delka_pulzu/1000000.0);
+    //tmp_delkova_rychlost = (pulz_na_metr * 1000000.0) / (delka_pulzu);
+    //tmp_delkova_rychlost = (pulz_na_metr_per_mil) / (delka_pulzu);
+    float tmp_cas_pulzu = tmp_celkova_delka_pulzu / my_pocetPulzu;
+    tmp_delkova_rychlost = (pulz_na_metr_per_mil) / (tmp_cas_pulzu);
+    if((korekcni_hranice_spodni < tmp_delkova_rychlost) && (tmp_delkova_rychlost < korekcni_hranice))
+    {
+      tmp_delkova_rychlost *= korekce2;
+    }
+
     if(smer==1)
     {
-      if (delkova_rychlost != 0) delkova_rychlost *= -1;
+      if (tmp_delkova_rychlost != 0) tmp_delkova_rychlost *= -1;
     }
+    /*
     if( 0 == (int)(rychlost*100) )
     {
-      delkova_rychlost = 0;
+      tmp_delkova_rychlost = 0;
     }
+    */
 
 /*
     char tmp_cpp[6];
@@ -217,19 +263,39 @@ void loop() {
 
     char tmp_delka[8];
     itoa(delka, tmp_delka, 7);
+
+    char del_rych_tmp[7];
+    dtostrf(tmp_delkova_rychlost, 4, 2, del_rych_tmp);
+    char delka_tmp[7];
+    float i = (double)delka/1000;
+    dtostrf(i, 4, 2, delka_tmp);
 */
     char tmp[40];
     //sprintf(tmp, "r:%s; s:%hi; d:%i; p:%i; cp:%i; c:%i;", rychlost_temp, synchronizace, delka, my_pocetPulzu, celkovy_pocet_pulzu, uplynulyCas);
     //sprintf(tmp, ";%s;%hi;%i;%i;", rychlost_temp, synchronizace, my_pocetPulzu, uplynulyCas);
-    sprintf(tmp, ";%s;%hi;%i;%i;", rychlost_temp, synchronizace, my_pocetPulzu, uplynulyCas);
+//    sprintf(tmp, ";%s;%hi;%i;%i;", rychlost_temp, synchronizace, my_pocetPulzu, uplynulyCas);
     //sprintf(tmp, ";%s;%hi;%i;%i;", rychlost_temp, synchronizace, my_pocetPulzu, (micros() - staryCasMicro));
+//    sprintf(tmp, ";%s;%hi;%i;%i;%i;%s;%s", rychlost_temp, synchronizace, my_pocetPulzu, uplynulyCas, celkovy_pocet_pulzu, delka_tmp, del_rych_tmp);
 
+    Serial.println(String("") + ";" + rychlost + ";"
+                                    + synchronizace +";"
+                                    + my_pocetPulzu +";"
+                                    + uplynulyCas +";"
+                                    + celkovy_pocet_pulzu +";"
+                                    + ((double)delka/1000) +";"
+                                    + tmp_delkova_rychlost +";"
+                                  );
+/*
     Serial.print(tmp);
     Serial.print(celkovy_pocet_pulzu);
     Serial.print(";");
     Serial.print((double)delka/1000);
     Serial.print(";");
-    Serial.println(delkova_rychlost);
+    Serial.print(tmp_delkova_rychlost);
+    Serial.print(";");
+    Serial.println(rychlost);
+*/
+
     //Serial.println(tmp_cpp);
 
     // uložení aktuálního času pro zahájení dalšího měření
@@ -321,6 +387,7 @@ void prictiPulz() {
   // inkrementace čítače pulzů
   flag_interrupt = 1;
   pocetPulzu++;
+  pompulz++;
 
   aktualniA = (bool)digitalRead(pinKanalA);
   aktualniB = (bool)digitalRead(pinKanalB);
@@ -329,44 +396,44 @@ void prictiPulz() {
        {
           if((aktualniB == false) && (predchoziB == false))
           {
-            smer = 1;
+            smer = 0;
           }
           else if((aktualniB == true) && (predchoziB == true))
           {
-            smer = 0;
+            smer = 1;
           }
        }
        else if((aktualniA == false) && (predchoziA == false))
        {
           if((aktualniB == true) && (predchoziB == false))
           {
-            smer = 1;
+            smer = 0;
           }
           else if((aktualniB == false) && (predchoziB == true))
           {
-            smer = 0;
+            smer = 1;
           }
        }
        else if((aktualniA == true) && (predchoziA == false))
        {
           if((aktualniB == true) && (predchoziB == true))
           {
-            smer = 1;
+            smer = 0;
           }
           else if((aktualniB == false) && (predchoziB == false))
           {
-            smer = 0;
+            smer = 1;
           }
        }
        else if((aktualniA == true) && (predchoziA == true))
        {
           if((aktualniB == false) && (predchoziB == true))
           {
-            smer = 1;
+            smer = 0;
           }
           else if((aktualniB == true) && (predchoziB == false))
           {
-            smer = 0;
+            smer = 1;
           }
        }
 
@@ -382,6 +449,7 @@ void prictiPulz() {
   */
   cas_pulzu = micros();
   delka_pulzu = cas_pulzu - predchozi_cas_pulzu;
+  celkova_delka_pulzu += delka_pulzu;
   predchozi_cas_pulzu = cas_pulzu;
 
 }
